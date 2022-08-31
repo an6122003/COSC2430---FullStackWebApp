8e1c373a1101baa18129e21affd6b815afcb00b4
gmthog <101091636+gmthog@users.noreply.github.com>
2022-08-31 16:03:05 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,4 @@
+<nav>
+    <a href="login.php">Login</a>
+    <a href="#">My Account</a>
+</nav>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="group" content="31">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/design.css">
+</head>
+<body>
+    <main>
+        <form method="post" action="index.php">
+            <input type="text" name="username" placeholder="User Name"> <br>
+            <input type="text" name="password" placeholder="Password"> <br>
+            <input type="submit" name="login" value="login"> 
+        </form>
+    </main>
+</body>
+</html>
